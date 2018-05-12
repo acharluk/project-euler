5866624249fd4e0bf47ade79572d383d956f8adb
ACharLuk <acharluk@gmail.com>
2018-05-12 15:45:22 +0200

    Remove extra output

--- lua/3_largest_prime_factor.lua
+++ lua/3_largest_prime_factor.lua
@@ -5,16 +5,13 @@ while number > factor do
     factor = (function(n)
         for i = 2, math.sqrt(n) do
             if n % i == 0 then
-                print("prime: ", i, n)
                 return i
             end
         end
-        print("last: ", n)
         return n
     end)(number)
 
     number = number / factor
-    print("Number / Factor:", number, factor)
 end
 
 print(factor)
